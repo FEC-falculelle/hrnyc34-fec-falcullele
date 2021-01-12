ca765f34c3d65accf028f418fb581fe7f7ea0144
Theodore Telonis <contacttheotelonis@gmail.com>
2021-01-12 18:49:46 -0500

    fixed file paths in imports

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { store } from './app/store';
+import './styles/index.css';
+import App from './components/App';
+import { store } from './store/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 
