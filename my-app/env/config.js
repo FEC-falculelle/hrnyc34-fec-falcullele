3044a8dd920f42d9b5140de627798ec048e84e5e
blargeth <albertkchua@gmail.com>
2021-01-15 10:44:36 -0500

    renders sample component for ratings and reviews

--- my-app/env/config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  TOKEN: 'bb4a772d7fa6682172fb9ec122dcf6ae40c50595'
-};
---
+++ my-app/src/components/widgets/ratings/ratingReview.tsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import {RootStore} from '../../../store/store';
+
+// import {getReviews} from '../../../actions/ratingReview/getReviewsAction';
+// import ImageGallery from './ImageGallery';
+// import ProductInfo from './ProductInfo';
+// import ProductDescription from './ProductDescription';
+
+interface Props {
+  exampleData: string;
+}
+
+function ProductReviewsComponent() {
+  // const dispatch = useDispatch();
+  // const productReviewsState = useSelector((state: RootStore) => state.productReviewsInfo);
+
+  // console.log('product state: ', productReviewsInfo);
+
+  return (
+    <div className='ratings'>
+      ratings and review component
+    </div>
+
+  );
+}
+
+export default ProductReviewsComponent;
