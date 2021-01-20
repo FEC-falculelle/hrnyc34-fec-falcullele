a7b19e49002ac47346d2483e387271c9959d0148
Theodore Telonis <contacttheotelonis@gmail.com>
2021-01-20 17:09:03 -0500

    fixed selected style check mark positioning

--- my-app/src/components/widgets/product/styleComponent.tsx
+++ my-app/src/components/widgets/product/styleComponent.tsx
@@ -11,13 +11,13 @@ function StyleComponent({style, clickFunction, index, curIndex}:Result | any) {
 
   return (
       isSelected ?
-      <div>
-        <div className='selectedStyle'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className='selectedStyleCheck' viewBox="0 0 16 16">
-  <path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z"/>
-</svg></div>
-        <div className='styleBubble' onClick={clickFunction} id={index}
-        style={{backgroundImage: `url(${style.photos[0].thumbnail_url})`}}>
-        </div>
+      <div className='styleBubble' onClick={clickFunction} id={index}
+      style={{backgroundImage: `url(${style.photos[0].thumbnail_url})`}}>
+           <div className='selectedStyle'>
+             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className='selectedStyleCheck' viewBox="0 0 16 16">
+            <path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z"/>
+            </svg>
+          </div>
       </div>
       :
       <div className='styleBubble' onClick={clickFunction} id={index}
