9383031017509e91954dfcf79f59d0ee1189809f
Theodore Telonis <contacttheotelonis@gmail.com>
2021-01-21 17:16:18 -0500

    created header and fixed reviews a tag

--- my-app/src/components/App.tsx
+++ my-app/src/components/App.tsx
@@ -33,7 +33,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Falculelle Gang #FEC</h1>
+        <h1>Logo</h1>
+        <div>
+          _____________
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"  viewBox="0 0 16 16">
+          <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+        </svg>
+        </div>
       </header>
       <Product />
       <div className='related-products'>
@@ -43,7 +49,7 @@ function App() {
         <h2>QUESTIONS &amp; ANSWERS</h2>
         <Questions />
       </div>
-      <div className='reviews'>
+      <div id='reviews' className='reviews'>
           product reviews
           <ProductReviewsComponent/>
       </div>
